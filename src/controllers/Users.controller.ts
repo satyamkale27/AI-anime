e9c99728ff42af3a80ece157730e75be5457b85f
satyam <satyamkale271@gmail.com>
2025-02-01 19:29:55 +0530

    login

--- src/controllers/Users.controller.ts
+++ src/controllers/Users.controller.ts
@@ -4,7 +4,7 @@ import User from "../models/Users.mongo";
 import asyncHandler from "../middlewares/tryCatch";
 import { CustomError } from "../middlewares/errors/CustomError";
 
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 
 export const registerUser = asyncHandler(
   async (req: Request, res: Response) => {
@@ -12,7 +12,22 @@ export const registerUser = asyncHandler(
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = new User({ username, password: hashedPassword });
     await user.save();
+    if (!user) throw new CustomError("unable to register", 500);
     res.status(201).json({ message: "User registered successfully" });
-    if (!user) return new CustomError("unable to register", 500);
+  }
+);
+
+export const loginUser = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) throw new CustomError("Authentication failed", 401);
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) throw new CustomError("Authentication failed", 401);
+
+    const token = jwt.sign({ userId: user._id }, "your-secret-key", {
+      expiresIn: "1h",
+    });
+    res.status(200).json({ token });
   }
 );
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,7 +1,10 @@
 import { Express } from "express";
 import { Router } from "express";
 import { registerUser } from "../controllers/Users.controller";
+import { loginUser } from "../controllers/Users.controller";
+
 const userRouter = Router();
 userRouter.post("/register", registerUser);
+userRouter.post("/login", loginUser);
 
 export default userRouter;
